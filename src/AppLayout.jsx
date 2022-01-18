b29bf3a622db51594121815baee31fab7fc798f2
James <jalindsay22@gmail.com>
2022-01-18 15:44:23 +0000

    comments

--- src/AppLayout.jsx
+++ src/AppLayout.jsx
@@ -1,20 +1,28 @@
-import { Button, Input, Table, Typography } from 'antd';
+import { Button, Input, Table } from 'antd';
 import React, { Component } from 'react';
 import axios from 'axios';
 
+// Main application react component
 class AppLayout extends Component {
     
   constructor() {
     super();
+
+    // Set initial state variables here
     this.state = {
       searchField: '',
       result: []
     }
 
+    // Set the index and titles of the table columns
     this.columns = [
       {
         title: 'Title',
         dataIndex: 'title',
+      },
+      {
+        title: 'Author',
+        dataIndex: 'author_name',
       },
       {
         title: 'Published',
@@ -23,7 +31,9 @@ class AppLayout extends Component {
   ];
   }
 
+  // Main react render function
   render() {
+
     return <>
       <Input 
         placeholder='enter book title' 
@@ -39,12 +49,14 @@ class AppLayout extends Component {
   }
 
   onSearchClick() {
-    console.log(this.state.searchField);
+
+    // Use axios to send the get request with the query from the text field appended
     axios.get('http://openlibrary.org/search.json?q=' + this.state.searchField)
       .then((response) => this.setState({result: response.data.docs}))
       .catch((error) => this.handleError(error));
   }
 
+  // Log caught error
   handleError(error) {
     console.log(error);
   }
