d0b5451f75a3e363965e14fa710b771cc0d8a22b
James <jalindsay22@gmail.com>
2022-01-19 13:00:27 +0000

    Loading indicator
    success function added
    readme description

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # programming excercise for Polestar
+
+Search for a book using the OpenLibrary API and display the results in a table
--- src/AppLayout.jsx
+++ src/AppLayout.jsx
@@ -1,4 +1,4 @@
-import { Button, Input, Table } from 'antd';
+import { Button, Input, Table, Row, Col } from 'antd';
 import React, { Component } from 'react';
 import axios from 'axios';
 
@@ -11,7 +11,8 @@ class AppLayout extends Component {
     // Set initial state variables here
     this.state = {
       searchField: '',
-      result: []
+      result: [],
+      loading: false
     }
 
     // Set the index and titles of the table columns
@@ -35,30 +36,50 @@ class AppLayout extends Component {
   render() {
 
     return <>
-      <Input 
-        placeholder='enter book title' 
-        value={this.state.searchField} 
-        onChange={e => this.setState({searchField: e.target.value})}
-      />
-      <Button onClick={() => this.onSearchClick()}>Search</Button>
+      <Row>
+        <Col span={6}>
+          <Input 
+            placeholder='enter book title' 
+            value={this.state.searchField} 
+            onChange={e => this.setState({searchField: e.target.value})}
+          />
+          <Button onClick={() => this.onSearchClick()}>Search</Button>
+        </Col>
+      </Row>
+      <Row>
+      <Col span={24}> 
       <Table 
         columns={this.columns}
         dataSource={this.state.result}
+        loading={this.state.loading}
       />
+      </Col>
+      </Row>
     </>
   }
 
+  // Function called when search buton is clicked
   onSearchClick() {
+
+    // Set loading to true so table will show loading indicator
+    this.setState({loading: true});
 
     // Use axios to send the get request with the query from the text field appended
     axios.get('http://openlibrary.org/search.json?q=' + this.state.searchField)
-      .then((response) => this.setState({result: response.data.docs}))
+      .then((response) => this.handleSuccess(response))
       .catch((error) => this.handleError(error));
   }
 
   // Log caught error
   handleError(error) {
+
     console.log(error);
+  }
+
+  // Handle successful GET request
+  handleSuccess(response) {
+
+    this.setState({result: response.data.docs, loading: false})
   }
 }
 
